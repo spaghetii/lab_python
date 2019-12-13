06c3b42f1c8e6483ca978e5238f7506221883186
spaghetii <joe.sun.m.j@gmail.com>
2019-12-13 11:56:51 +0800

    just practice python

---
+++ hello.py
@@ -0,0 +1,34 @@
+import sys
+import turtle
+#import this
+
+"""
+我是註解
+"""
+
+# python版本資訊
+print(sys.version_info)
+# 版本
+print(sys.version)
+
+print("hello python")
+# sep
+print('hello', 'world', sep=', ', end='!')
+
+print('goodbye, world', end='!\n=======================================\n')
+
+'''
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
+'''
+
+
+
